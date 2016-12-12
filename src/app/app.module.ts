00cbe3c53e215fa11969e615cd5591d6e50fa554
Jaime Niswonger <jniswonger@keyholesoftware.com>
2016-12-12 14:10:49 -0600

    prepare for initial devlopment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
-import { AppComponent } from './app.component';
+import {NgModule} from '@angular/core';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [],
+    imports: [],
+    providers: [],
+    bootstrap: []
 })
-export class AppModule { }
+export class AppModule {
+}
