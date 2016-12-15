f60a365644f8e612d54702801b4b9dd92532553d
Jaime Niswonger <jniswonger@keyholesoftware.com>
2016-12-15 15:26:09 -0600

    added filtering input

--- src/app/movie/movie.module.ts
+++ src/app/movie/movie.module.ts
@@ -2,11 +2,13 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HttpModule} from '@angular/http';
 import {MoviesComponent} from './movies.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
     imports: [
         CommonModule,
-        HttpModule
+        HttpModule,
+        FormsModule
     ],
     declarations: [
         MoviesComponent
--- src/app/movie/movies.component.ts
+++ src/app/movie/movies.component.ts
@@ -7,8 +7,9 @@ import {MovieService} from './movie.service';
     template: `
         <div class="movie-container">
             <h2>Now Playing</h2>
+            <input type="text" placeholder="Filter..." [(ngModel)]="filterText" (keyup)="applyFilter()"/>
             <ul>
-                <li *ngFor="let movie of movies">
+                <li *ngFor="let movie of filteredMovies">
                     <img src="/assets/images/posters{{movie.poster_path}}"/>
                 </li>
             </ul>
@@ -19,13 +20,22 @@ import {MovieService} from './movie.service';
 export class MoviesComponent implements OnInit {
 
     private movies: Movie[];
+    private filteredMovies: Movie[];
+    private filterText: string;
 
     constructor(private movieService: MovieService) {
     }
 
     ngOnInit(): void {
-        this.movieService.nowPlaying().subscribe(movies =>
-            this.movies = movies
+        this.movieService.nowPlaying().subscribe(movies => {
+            this.movies = movies;
+            this.filteredMovies = movies;
+        })
+    }
+
+    applyFilter(): void {
+        this.filteredMovies = this.movies.filter(movie =>
+            movie.title.toLowerCase().match(this.filterText.toLowerCase()) != null
         )
     }
 }
