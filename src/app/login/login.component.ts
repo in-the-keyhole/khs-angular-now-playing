b173f2a608d41d5fb8440e747223c6f495888d04
Jaime Niswonger <jniswonger@keyholesoftware.com>
2016-12-27 14:32:33 -0600

    changed to reactive form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit}                  from '@angular/core';
+import {FormGroup, FormBuilder, Validators} from '@angular/forms';
 import {User} from './user.model';
 import {Router} from '@angular/router';
 
@@ -8,19 +9,64 @@ import {Router} from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-    private errorMessage: string;
     private user: User;
+    public loginForm: FormGroup;
 
-    constructor(private router: Router) {
+    constructor(private fb: FormBuilder, private router: Router) {
     }
 
     ngOnInit(): void {
         this.user = new User();
+        this.buildForm();
     }
 
-    onSubmit(): void {
+    onSubmit() {
+        this.user = this.loginForm.value;
         // For now, just log the user
         console.log('Login for ->', this.user);
         this.router.navigate(['']);
     }
+
+    buildForm(): void {
+        this.loginForm = this.fb.group({
+            'username': [this.user.username, [
+                Validators.required,
+                Validators.minLength(4)
+            ]],
+            'password': [this.user.password, Validators.required]
+        });
+        this.loginForm.valueChanges.subscribe(data => this.onValueChanged(data));
+    }
+
+    onValueChanged(data?: any) {
+        if (!this.loginForm) {
+            return;
+        }
+        const form = this.loginForm;
+        for (const field in this.formErrors) {
+            // clear previous error message (if any)
+            this.formErrors[field] = '';
+            const control = form.get(field);
+            if (control && control.dirty && !control.valid) {
+                const messages = this.validationMessages[field];
+                for (const key in control.errors) {
+                    this.formErrors[field] += messages[key] + ' ';
+                }
+            }
+        }
+    }
+
+    formErrors = {
+        'username': '',
+        'password': ''
+    };
+    validationMessages = {
+        'username': {
+            'required': 'Username is required.',
+            'minlength': 'Username must be at least 4 characters long.'
+        },
+        'password': {
+            'required': 'Password is required.'
+        }
+    };
 }
